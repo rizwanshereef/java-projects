782f403fab69c50854f145276c4223cd29241b49
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-21 11:17:21 +0000

    Initial Commit

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a =20;
+        int b= 5;
+        
+        System.out.println("welcome" +
+                "");
+
+
+    }
+}
